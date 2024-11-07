4caeb1a12cda6197188895272bbdc7c5eba96e4f
julpermaz <julpermaz@alu.edu.gva.es>
2024-11-07 11:44:57 +0100

    inverso

---
+++ src/main/Ejercicios_arrays2/Inverso.java
@@ -0,0 +1,19 @@
+package Ejercicios_arrays2;
+
+import java.util.Arrays;
+
+public class Inverso {
+    public static String inversion(int[] original){
+        int[] inverso = new int[original.length];
+
+        for (int i = 0; i < original.length; i++) {
+            inverso[i] = original[original.length - 1 - i];
+        }
+        return Arrays.toString(inverso);
+    }
+
+    public static void main(String[] args) {
+        int[] numeros;
+
+    }
+}
